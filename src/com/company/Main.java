4fc83be7f5fadb6696c5d66404f1d5b2f4b76cd7
badya367 <wildman367@gmail.com>
2020-10-21 19:30:27 +0400

    Задание на написание javadoc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
-public class Main {
+/**
+ * Class Main создан в обучающих целях
+ * Method fact - считает факториал числа
+ * Method abs - модуль числа
+ * Method pow - возведение в степень
+ * @author BadikovDA
+ * @version 1.0.
+ */
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
+public class Main{
+    static int fact(int n){
+        int result = 1;
+        for (int i = 1; i <=n; i ++){
+            result = result*i;
+        }
+        return result;
+    }
+
+    static int abs(int n) {
+        if (n<0) {
+            return -n;
+        }
+        else {
+            return n;
+        }
+    }
+
+    public static int pow(int n){
+        return n*n;
+    }
+
+    public static void main(String[] args){
+        System.out.println(fact(10));
+        System.out.println(abs(10));
+        System.out.println(pow(2));
     }
 }
